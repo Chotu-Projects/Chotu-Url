cf28fe27df0a92c15b8b5f0f98e9c8e2ebcc241e
Punit Choudhary <itspunitchoudhary@gmail.com>
2022-09-23 22:27:05 +0530

    Linted

--- Database/database.py
+++ Database/database.py
@@ -12,30 +12,30 @@ def connect(mongo_uri, database, collection_name):
     Connect to mongodb database and return collection
     """
 
-    with console.status("[bright_magenta]Connecting to DataBase...") as status:
+    with console.status("[bright_magenta]Connecting to DataBase...") as _:
         try:
             client = MongoClient(mongo_uri)
-            console.log(f"[bright_green]Fetched Client[/bright_green]✅")
-        except Exception as e:
+            console.log("[bright_green]Fetched Client[/bright_green]✅")
+        except Exception as error:
             console.log(
-                f"[bright_red]Error occured while fetching Client[/bright_red]❌")
-            console.log(f"Error: {e}")
+                "[bright_red]Error occured while fetching Client[/bright_red]❌")
+            console.log(f"Error: {error}")
 
         try:
             db = client[database]
-            console.log(f"[bright_green]Fetched DataBase[/bright_green]✅")
-        except Exception as e:
+            console.log("[bright_green]Fetched DataBase[/bright_green]✅")
+        except Exception as error:
             console.log(
-                f"[bright_red]Error occured while fetching Database[/bright_red]❌")
-            console.log(f"Error: {e}")
+                "[bright_red]Error occured while fetching Database[/bright_red]❌")
+            console.log(f"Error: {error}")
 
         try:
             collection = db[collection_name]
-            console.log(f"[bright_green]Fetched Collection[/bright_green]✅")
-        except Exception as e:
+            console.log("[bright_green]Fetched Collection[/bright_green]✅")
+        except Exception as error:
             console.log(
-                f"[bright_red]Error occured while fetching Collection[/bright_red]❌")
-            console.log(f"Error: {e}")
+                "[bright_red]Error occured while fetching Collection[/bright_red]❌")
+            console.log(f"Error: {error}")
 
     return collection
 
@@ -51,21 +51,21 @@ def setup(collection, expire_days: int, inactive_days: int) -> None:
     try:
         collection.create_index("expireAt", expireAfterSeconds=expire_seconds)
         console.log(
-            f"[bright_green]Successfully created expireAt index[/bright_green]✅")
-    except Exception as e:
+            "[bright_green]Successfully created expireAt index[/bright_green]✅")
+    except Exception as error:
         console.log(
-            f"[bright_red]Error occured while creating expireAt index[/bright_red]❌")
-        console.log(f"Error: {e}")
+            "[bright_red]Error occured while creating expireAt index[/bright_red]❌")
+        console.log(f"Error: {error}")
 
     try:
         collection.create_index(
             'expireInactiveAt', expireAfterSeconds=inactive_seconds)
         console.log(
-            f"[bright_green]Successfully created expireInactiveAt index[/bright_green]✅")
-    except Exception as e:
+            "[bright_green]Successfully created expireInactiveAt index[/bright_green]✅")
+    except Exception as error:
         console.log(
-            f"[bright_red]Error occured while creating expireInactiveAt index[/bright_red]❌")
-        console.log(f"Error: {e}")
+            "[bright_red]Error occured while creating expireInactiveAt index[/bright_red]❌")
+        console.log(f"Error: {error}")
 
 
 def exists(collection, url_id: str) -> bool:
@@ -75,8 +75,7 @@ def exists(collection, url_id: str) -> bool:
     data = collection.find_one({"_id": url_id})
     if data:
         return True
-    else:
-        return False
+    return False
 
 
 def store(collection, chotu_url: str, original_url: str) -> bool:
@@ -118,11 +117,13 @@ def update_view(collection, chotu_url, value=1, extend_date=datetime.utcnow()):
                 }
             )
             console.log(
-                f"[bright_green]Successfully updated view and expireInactiveAt for `{chotu_url}[/bright_green]✅")
-        except Exception as e:
+                f"[bright_green]Successfully updated view and expireInactiveAt \
+                    for `{chotu_url}[/bright_green]✅")
+        except Exception as error:
             console.log(
-                f"[bright_red]Error occured while updating view or expireInactiveAt for `{chotu_url}`[/bright_red]❌")
+                f"[bright_red]Error occured while updating view or expireInactiveAt \
+                    for `{chotu_url}`[/bright_red]❌")
+            console.log(f"Error: {error}")
 
         return True
-    else:
-        return False
+    return False
