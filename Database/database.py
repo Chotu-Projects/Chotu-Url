9e7aaefa0352d85a41b4093fdfc4e5614460632f
Punit Choudhary <itspunitchoudhary@gmail.com>
2022-09-23 22:05:18 +0530

    v1 database - create update_view function

--- Database/database.py
+++ Database/database.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from dotenv import load_dotenv
 from pymongo import MongoClient
 from rich.console import Console
 
-load_dotenv()
+# setup Python-Rich
 console = Console()
+
 
 def connect(mongo_uri, database, collection_name):
     """
@@ -17,27 +17,30 @@ def connect(mongo_uri, database, collection_name):
             client = MongoClient(mongo_uri)
             console.log(f"[bright_green]Fetched Client[/bright_green]✅")
         except Exception as e:
-            console.log(f"[bright_red]Error occured while fetching Client[/bright_red]❌")
+            console.log(
+                f"[bright_red]Error occured while fetching Client[/bright_red]❌")
             console.log(f"Error: {e}")
-        
+
         try:
             db = client[database]
             console.log(f"[bright_green]Fetched DataBase[/bright_green]✅")
         except Exception as e:
-            console.log(f"[bright_red]Error occured while fetching Database[/bright_red]❌")
+            console.log(
+                f"[bright_red]Error occured while fetching Database[/bright_red]❌")
             console.log(f"Error: {e}")
-        
+
         try:
             collection = db[collection_name]
             console.log(f"[bright_green]Fetched Collection[/bright_green]✅")
         except Exception as e:
-            console.log(f"[bright_red]Error occured while fetching Collection[/bright_red]❌")
+            console.log(
+                f"[bright_red]Error occured while fetching Collection[/bright_red]❌")
             console.log(f"Error: {e}")
 
     return collection
 
 
-def setup(collection, expire_days:int, inactive_days:int) -> None:
+def setup(collection, expire_days: int, inactive_days: int) -> None:
     """
     Create Index to automatically drop documents
     """
@@ -47,20 +50,28 @@ def setup(collection, expire_days:int, inactive_days:int) -> None:
 
     try:
         collection.create_index("expireAt", expireAfterSeconds=expire_seconds)
-        console.log(f"[bright_green]Successfully created expireAt index[/bright_green]✅")
+        console.log(
+            f"[bright_green]Successfully created expireAt index[/bright_green]✅")
     except Exception as e:
-        console.log(f"[bright_red]Error occured while creating expireAt index[/bright_red]❌")
+        console.log(
+            f"[bright_red]Error occured while creating expireAt index[/bright_red]❌")
         console.log(f"Error: {e}")
-    
+
     try:
-        collection.create_index('expireInactiveAt', expireAfterSeconds=inactive_seconds)
-        console.log(f"[bright_green]Successfully created expireInactiveAt index[/bright_green]✅")
+        collection.create_index(
+            'expireInactiveAt', expireAfterSeconds=inactive_seconds)
+        console.log(
+            f"[bright_green]Successfully created expireInactiveAt index[/bright_green]✅")
     except Exception as e:
-        console.log(f"[bright_red]Error occured while creating expireInactiveAt index[/bright_red]❌")
+        console.log(
+            f"[bright_red]Error occured while creating expireInactiveAt index[/bright_red]❌")
         console.log(f"Error: {e}")
 
 
-def exists(collection, url_id:str) -> bool:
+def exists(collection, url_id: str) -> bool:
+    """
+    Checks for availability of url_id in database
+    """
     data = collection.find_one({"_id": url_id})
     if data:
         return True
@@ -68,11 +79,14 @@ def exists(collection, url_id:str) -> bool:
         return False
 
 
-def store(collection, chotu_url:str, original_url:str) -> bool:
-    
+def store(collection, chotu_url: str, original_url: str) -> bool:
+    """
+    Create and store new document/entry for chotu_url
+    """
+
+    # check for availability
     if exists(collection, chotu_url):
         return False
-
 
     document = {
         "_id": chotu_url,
@@ -81,17 +95,34 @@ def store(collection, chotu_url:str, original_url:str) -> bool:
         "views": 0,
         "createdAt": datetime.utcnow(),
         "expireAt": datetime.utcnow(),
-        "expireFirst": datetime.utcnow()
+        "expireInactiveAt": datetime.utcnow()
     }
 
     collection.insert_one(document)
     return True
 
 
-def update_view(collection, chotu_url, value=1, extend_date=):
-    pass
+def update_view(collection, chotu_url, value=1, extend_date=datetime.utcnow()):
+    """
+    Update the view of url and extends the expire-inactive
+    """
+    # check if url exists
+    if exists(collection, chotu_url):
+        # update the view
+        try:
+            collection.update_one(
+                {"_id": chotu_url}, {
+                    # increment views
+                    "$inc": {"views": value},
+                    "$set": {"expireInactiveAt": extend_date}
+                }
+            )
+            console.log(
+                f"[bright_green]Successfully updated view and expireInactiveAt for `{chotu_url}[/bright_green]✅")
+        except Exception as e:
+            console.log(
+                f"[bright_red]Error occured while updating view or expireInactiveAt for `{chotu_url}`[/bright_red]❌")
 
-# TEST
-# collection = connect()
-# status = store(collection, 'gogo', 'https://google.com')
-# print(status)
+        return True
+    else:
+        return False
